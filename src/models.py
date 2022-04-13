feff043cec80f4771b317f14c40b82dd5c716d10
BenjaminNassW <benjanass@gmail.com>
2022-04-13 00:08:46 +0000

    codigo listo

--- src/models.py
+++ src/models.py
@@ -8,26 +8,76 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    character_name = Column(String(250))
+    gender = Column(String(250))
+    homeworld = Column(String(250))
+    user = relationship(User)
+
+
+class Location(Base):
+    __tablename__ = 'location'
+    id = Column(Integer, primary_key=True)
+    location_name = Column(String(250), ForeignKey(
+        'character.homeworld'))
+    terrain = Column(String(250))
+    population = Column(Integer())
+    character = relationship(Character)
+    user = relationship(User)
+
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key=True)
+    starship_name = Column(String(250))
+    crew = Column(Integer())
+    model = Column(String(250))
+    user = relationship(User)
+
+
+class Fav_C(Base):
+    __tablename__ = 'fav_c'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(250), ForeignKey('user.name'))
+    character_name = Column(
+        String(250), ForeignKey('character.character_name'))
+    user = relationship(User)
+    character = relationship(Character)
+
+
+class Fav_L(Base):
+    __tablename__ = 'fav_l'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(250), ForeignKey('user.name'))
+    character_name = Column(
+        String(250), ForeignKey('location.location_name'))
+    user = relationship(User)
+    location = relationship(Location)
+
+
+class Fav_S(Base):
+    __tablename__ = 'fav_s'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(250), ForeignKey('user.name'))
+    character_name = Column(
+        String(250), ForeignKey('starship.starship_name'))
+    user = relationship(User)
+    starship = relationship(Starship)
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
